261b49ec68406abe9fa54e8e5bf44abb46cc764d
jumpyguest <roland.almendras.salazar@gmail.com>
2025-02-04 14:03:55 +0800

    Implement handling of non-alphabet characters

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,14 +1,19 @@
 def caesar_cipher(string, shift_factor)
   array_output = Array.new
   string.each_char do |char|
-    if is_upper?(char)
-      new_char = encrypt_char(char.downcase, shift_factor)
-      array_output.push(new_char.upcase)
+    if ((char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z'))
+      if is_upper?(char)
+        new_char = encrypt_char(char.downcase, shift_factor)
+        array_output.push(new_char.upcase)
+      else
+        new_char = encrypt_char(char, shift_factor)
+        array_output.push(new_char)
+      end
     else
-      new_char = encrypt_char(char, shift_factor)
-      array_output.push(new_char)
+      array_output.push(char)
     end
   end
+  p array_output.join
 end
 
 def encrypt_char(ch, shift_factor)
@@ -23,4 +28,4 @@ def is_upper?(ch)
   ch == ch.upcase
 end
 
-caesar_cipher("Hen", 2)
+caesar_cipher("What a string!", 5)
